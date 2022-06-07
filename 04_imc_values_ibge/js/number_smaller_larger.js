22ec611f760a1a18b946d3fc09b30c5efe0eee8c
Felipe Ozias <oziasfelipe38@gmail.com>
2022-06-07 20:42:15 -0300

    feat: new page number_smaller_larger

---
+++ 04_imc_values_ibge/js/number_smaller_larger.js
@@ -0,0 +1,22 @@
+// import the elements
+const btn_sort = document.querySelector('#btn_calc');
+const result = document.querySelector('#result');
+
+// function smaller and larger number
+function calc() {
+
+    const number = parseFloat(document.querySelector('#number_float').value.replace(',','.'));
+
+    // Condition
+    if (isNaN(number) === true) {
+        result.textContent = ('O valor inserido não é numérico!');
+    } else {
+        const smaller = Math.floor(number);
+        const larger = Math.ceil(number);
+
+        result.textContent = `O menor número inteiro é ${smaller} e o maior número inteiro é ${larger}`;
+
+    }
+}
+
+btn_sort.onclick = calc;
