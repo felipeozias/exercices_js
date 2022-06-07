d265b4c0370f5bf813146d1c11f4bbb9e4e366f3
Felipe Ozias <oziasfelipe38@gmail.com>
2022-06-07 19:28:57 -0300

    feat: new page random_number

---
+++ 04_imc_values_ibge/js/random_number.js
@@ -0,0 +1,27 @@
+// import the elements
+const btn_sort = document.querySelector('#btn_calc');
+const result = document.querySelector('#result');
+
+// function raffle number
+function calc() {
+
+    const val_min = parseInt(document.querySelector('#val_min').value);
+    const val_max = parseInt(document.querySelector('#val_max').value);
+
+    // Condition
+    if ((isNaN(val_min) === true) || (isNaN(val_max) === true)) {
+        result.textContent = ('Os valores inseridos não são numéricos inteiros!');
+    } else {
+
+        if (val_min >= val_max) {
+            result.textContent = 'O valor mínimo não é menor que o valor máximo!';
+
+        } else {
+            let aleatory = parseInt(Math.random() * (val_max - val_min) + val_min);
+            result.textContent = aleatory;
+        }
+    }
+
+}
+
+btn_sort.onclick = calc;
